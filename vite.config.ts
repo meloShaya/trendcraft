42d041f0700e93b9726106790e7db51696b16476
Melo Shaya <122815236+meloShaya@users.noreply.github.com>
2025-06-20 12:27:25 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,10 +9,8 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
